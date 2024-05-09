0ec1aee690fdadeb9664209ae9162e57d5df2a35
antlers <antlers@illucid.net>
2024-05-09 16:33:32 -0700

    Split hands further apart

--- magic-strdy.scm
+++ magic-strdy.scm
@@ -11,8 +11,9 @@
              (ice-9 textual-ports))
 
 (define alphabet
-  '(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n
-    #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z #\. #\,))
+  '(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m
+    #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z
+    #\. #\, #\/))
 
 (define aliases
   (string-join
